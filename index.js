5dfb2ecac2a94ebb77f038f98341af90fed14d28
CHANDRAMUKHII <chandramukhiianbarasu@gmail.com>
2022-07-22 08:29:30 +0530

    EDIT

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 const Product = require("./models/product");
 mongoose
@@ -13,14 +14,55 @@ mongoose
     console.log("OH NO MONGO CONNECTION ERROR!!!");
     console.log(err);
   });
-app.set();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.listen(3000, () => {
-  console.log("App is listening on port 3000");
-});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
-  res.send("Woof");
+  res.send("WELCOME TO THE FARM:)");
+});
+
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  res.render("products/index", { products });
+});
+
+app.get("/products/new", (req, res) => {
+  res.render("products/new");
+});
+
+app.post("/products/new", async (req, res) => {
+  const p = new Product(req.body);
+  await p.save();
+  console.log(p);
+  res.redirect(`/products/${p._id}`);
+  // res.render("/products");
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.put("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
+});
+
+app.listen(3000, () => {
+  console.log("App is listening on port 3000");
 });
